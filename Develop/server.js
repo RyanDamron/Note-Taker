689846799f79ccafa7135f32e89790836535cac3
RyanDamron <119913449+RyanDamron@users.noreply.github.com>
2023-02-07 23:44:07 -0600

    created server.js file

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const api = require('./routes/index.js');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
