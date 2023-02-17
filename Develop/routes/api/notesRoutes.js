b8bcb872b042ab92c07e8d5fce0bc17bf5015f20
RyanDamron <119913449+RyanDamron@users.noreply.github.com>
2023-02-16 18:47:06 -0600

    finished

--- Develop/routes/api/notesRoutes.js
+++ Develop/routes/api/notesRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId({ length: 10 });
 
 router.get('/', (req, res) => {
     fs.readFile('./db/db.json', function(err, data){
@@ -10,9 +12,18 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req,res) => {
-    res.json('hello')
-    let note = req.body
-    console.log(note);
+    const {title, text} = req.body
+    const updatedNote = {title, text, id: uid()}
+    console.log(updatedNote);
+    fs.readFile('./db/db.json', function(err, data){
+        if(err) throw err
+        let oldNotes = JSON.parse(data)
+        oldNotes.push(updatedNote)
+        fs.writeFile('./db/db.json', JSON.stringify(oldNotes), function(err){
+            if(err) throw err
+            res.json(oldNotes)
+        })
+    });
 });
 
 module.exports = router;
